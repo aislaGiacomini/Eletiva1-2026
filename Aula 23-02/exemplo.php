7ed555f4a23c758eeeea397115197c796490b7a4
aislaGiacomini <aislagsp@gmail.com>
2026-02-23 20:30:21 -0300

    Implementação do PHP dentro do HTML

---
+++ Aula 23-02/exemplo.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Primeiro exemplo PHP</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Primeiro exemplo PHP</h1>
+<h3> Data de hoje <?php echo date("d/m/y"); ?> <h3> 
+<form method="post" action="exemplo.php">
+<div class="mb-3">
+              <label for="Nome" class="form-label">Informe o seu nome </label>
+              <input type="text" id="nome" name="nome" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+  if($_SERVER['REQUEST_METHOD'] == 'POST'){
+    $nome = $_POST['nome'];
+    echo "<p> Seja bem vindo $nome </p>";
+  }
+    
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
