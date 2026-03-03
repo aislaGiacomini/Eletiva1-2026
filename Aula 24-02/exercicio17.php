086b3115a24db8127abe2ed881c13a1017384017
aislaGiacomini <aislagsp@gmail.com>
2026-03-03 15:09:46 -0300

    Exercicio 17 da lista de PHP - Calculo de juros simples

---
+++ Aula 24-02/exercicio17.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercicio 17</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercicio 17</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="valorCap" class="form-label">Insira o valor do capital</label>
+              <input type="number" id="valorCap" name="valorCap" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="taxaJuros" class="form-label">Insira a taxa de juros</label>
+              <input type="number" id="taxaJuros" name="taxaJuros" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="periodo" class="form-label">Insira o valor do periodo</label>
+              <input type="number" id="periodo" name="periodo" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+    if($_SERVER ['REQUEST_METHOD'] == 'POST'){
+        $valorCap = $_POST['valorCap'];
+        $taxaJuros = $_POST['taxaJuros'] / 100;
+        $periodo = $_POST['periodo'];
+
+        $resultado = $valorCap * $taxaJuros * $periodo;
+        echo "O resultado do juros simples é $resultado";
+    }
+?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
