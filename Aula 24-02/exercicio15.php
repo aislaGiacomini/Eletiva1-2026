cc6618706dbf80fad46a10c9cc60d7354563cac3
aislaGiacomini <aislagsp@gmail.com>
2026-03-03 14:45:24 -0300

    Exercicio 15 lista de PHP - calculo de IMC

--- Aula 24-02/exercicio15.php
+++ Aula 24-02/exercicio15.php
@@ -23,9 +23,10 @@
     if($_SERVER['REQUEST_METHOD'] == 'POST'){
         $numPeso = $_POST['numPeso'];
         $numAlt = $_POST['numAlt'];
+        $numAlt = $numAlt / 100;
         $valorImc = $numPeso / ($numAlt ** 2);
 
-        echo "O valor do IMC é: $valorImc";
+        echo "O valor do IMC é: ". number_format($valorImc, 2);
     }
 ?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
