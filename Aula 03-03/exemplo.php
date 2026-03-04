cf25ca6fcf0c33c89334f78f14ab656e653ce1b8
aislaGiacomini <aislagsp@gmail.com>
2026-03-04 16:18:49 -0300

    Exercicio 2 da lista de bootstrap

---
+++ Aula 03-03/exemplo.php
@@ -0,0 +1,64 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exemplo Estruturas de controle e repetição</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+    <div class="container py-3">
+    <h1>Exemplo Estruturas de controle e repetição</h1>
+    <form method="post">
+    <div class="mb-3">
+                <label for="valor" class="form-label">Informe um valor: </label>
+                <input type="text" id="valor" name="valor" class="form-control" required="">
+                </div>
+    <button type="submit" class="btn btn-primary">Enviar</button>
+    </form>
+    <?php
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $valor = $_POST['valor'];
+            /*if($valor == "+")
+                echo "<p>Sinal de soma!</p>";
+
+            elseif($valor == "-")
+                echo "<p>Sinal de subtração!</p>";
+            elseif($valor == "*"){
+                echo "<p>Sinal de multiplicação! </p>";
+            }
+            elseif ($valor == "/"){
+                echo "<p>Sinal de divisão!</p>";
+            }
+            else {
+                echo "<p> Sinal inválido! </p>";
+            }
+            
+            switch($valor){
+                case "+":
+                    echo "<p> Sinal de soma!</p>";
+                break;
+                case "-":
+                    echo "<p> Sinal de subtração!</p>";
+                    break;
+                case "*":
+                    echo "<p> Sinal de multiplicação!</p>";
+                    break;
+                case "/":
+                    echo "<p> Sinal de divisão!</p>";
+                    break;   
+                default:
+                    echo "<p>Sinal invalido</p>";                        
+            }
+            */
+
+            for($i = 1; $i <= 10; $i++){
+                $result = $valor * $i;
+                echo "<p> $valor * $i = $result <?p>";
+            }
+        }
+    ?>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+    </div>
+</body>
+</html>
