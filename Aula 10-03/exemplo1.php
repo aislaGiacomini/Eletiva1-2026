b085d1f0bd378bf69b6e369eb57fd1f0209643e4
aislaGiacomini <aislagsp@gmail.com>
2026-03-10 19:51:07 -0300

    Exemplo de funções em PHP

--- Aula 10-03/exemplo1.php
+++ Aula 10-03/exemplo1.php
@@ -1,4 +1,30 @@
 <?php
-    $data = date("d/m/Y h:i:s");
+    //Função de data e hora
+    date_default_timezone_set('America/Sao_Paulo');
+    $data = date("d/m/Y H:i:s");
     echo "<p>$data</p>";
+
+    $valor = 5.8;
+    $valorArredondado = round($valor); //arredondar para valor mais próximo 
+    echo "Valor arredondado: $valorArredondado";
+
+    $valorFormatado = number_format($valor, 2, ",","."); //formatar valor
+    echo "<p>Valor formatado: $valorFormatado </p>";
+
+    $exp = pow(3,4);// Exponenciação
+    $raiz = sqrt(16); //Raiz Quadrada
+    $aleatorio =rand(1, 100); //numero aleatório
+
+    if(isset($nome)){ //verificar existencia
+        echo "<p> Nome Informado!</p>";
+    } else {
+        echo "<p> Nome não informado </p>";
+        die(); //A aplicação acaba
+    }
+
+    if(is_float($valor)){ //verificar tipo 
+        echo "<p> É um numero flutuante</p>";
+    } else {
+        echo "<p> Não é um numero flutuante </p>";
+    }
 ?>
