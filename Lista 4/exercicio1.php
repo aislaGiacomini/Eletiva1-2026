063c44aa32ec00fd0a48d0a831c023afbc2e7cd8
aislaGiacomini <aislagsp@gmail.com>
2026-03-17 20:21:33 -0300

    Exercicio 1 e 2 da lista 4

--- Lista 4/exercicio1.php
+++ Lista 4/exercicio1.php
@@ -19,7 +19,7 @@
         <?php
             if($_SERVER['REQUEST_METHOD'] == "POST"){
                 $palavra = $_POST['palavra'];
-                $qtd = $palavra.lenth()
+                echo "Quatidade de caracteres: ". strlen($palavra);
             }
         ?>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
