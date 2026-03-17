0c204ef62737ece7548f7b1544cbc3d5c19923c4
aislaGiacomini <aislagsp@gmail.com>
2026-03-17 20:36:30 -0300

    Exercicio 4 da lista 4 - Função para data

---
+++ Lista 4/exercicio4.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercicio 4</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercicio 4</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="dia" class="form-label">Digite o dia</label>
+              <input type="number" id="dia" name="dia" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="mes" class="form-label">Digite o mês</label>
+              <input type="number" id="mes" name="mes" class="form-control" required="">
+            </div><div class="mb-3">
+              <label for="ano" class="form-label">Digite o ano</label>
+              <input type="number" id="ano" name="ano" class="form-control" required="">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+<?php
+            if($_SERVER['REQUEST_METHOD'] == "POST"){
+                $dia = $_POST['dia'];
+                $mes = $_POST['mes'];
+                $ano = $_POST['ano'];
+                if(checkdate($mes, $dia, $ano)){
+                    $date = mktime(0, 0, 0, $mes, $dia, $ano);
+                    echo date("d-m-Y", $date);
+                }
+                else{
+                    //echo "<p>A palavra $palavra2 não esta contida em $palavra </p>";
+                }
+
+            }
+        ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
