31313bf13b427c9a3b5a083bed0abae5f798f403
aislaGiacomini <aislagsp@gmail.com>
2026-03-17 21:08:41 -0300

    Exercicio 6 da lista 4 - Arredondamento

---
+++ Lista 4/exercicio6.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Exercicio 6</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+</head>
+<body> 
+<div class="container py-3">
+<h1>Exercicio 6</h1>
+<form method="post">
+<div class="mb-3">
+              <label for="num1" class="form-label">Insira um numero </label>
+              <input type="number" id="num1" name="num1" class="form-control" required="" step="any">
+            </div>
+<button type="submit" class="btn btn-primary">Enviar</button>
+</form>
+    <?php
+            if($_SERVER['REQUEST_METHOD'] == "POST"){
+                $num1 = $_POST['num1'];
+                echo "<p>Numero arredondado para cima: ".ceil($num1)."</p>";
+                echo "<p>Numero arredondado para baixo: ".floor($num1)."</p>";
+                echo "<p>Numero arredondado para o mais próximo: ".round($num1)."</p>";
+            }
+    ?>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
