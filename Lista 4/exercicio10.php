10c1fd0eeaa4eb6b62e8932cfccf7253db82059f
aislaGiacomini <aislagsp@gmail.com>
2026-03-19 20:32:17 -0300

    Exercicio 10 da lista 4 - Retirar iniciais de um nome

--- Lista 4/exercicio10.php
+++ Lista 4/exercicio10.php
@@ -16,6 +16,20 @@
             </div>
 <button type="submit" class="btn btn-primary">Enviar</button>
 </form>
+<?php
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        $nomeCompleto = $_POST['nomeCompleto'];
+        $semEspaco = trim($nomeCompleto);
+        $nome = strtok($semEspaco, " ");
+        $iniciais = '';
+        while($nome != false){
+          $iniciais .= strtoupper(substr($nome, 0, 1,)) . ".";
+          $nome = strtok(" ");
+        }
+
+        echo "<p>As inicias são $iniciais </p>";
+    }
+?>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
 </div>
 </body>
